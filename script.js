0420dee78e37d99350ebcbfc3fabebf65b6dc13d
Lindy Briggs <briggs.lindy@yahoo.com>
2022-04-12 11:29:01 -0500

    Button click event listener added, triggers fetches.

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
 let apiKey = `AIzaSyBzlcyOrjLwEnxvKDzqolIoMFkY_beQBSg`;
 let videoLinkEl = document.getElementById("link")
 let wikiFactEl = document.getElementById("wiki")
+let searchButton = document.querySelector("#search")
 
+searchButton.addEventListener("click", function (event) {
+    event.preventDefault();
 
-fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=YouTube+Data+API&type=video&videoCaption=closedCaption&key=${apiKey}`)
- .then(function (response) {
-   return response.json();
- })
- .then(function (data) {
-    let videoLink = "https://www.youtube.com/watch?v=" + data.items[0].id.videoId;
-    videoLinkEl.innerHTML = videoLink;
-    videoLinkEl.setAttribute("href",videoLink)
-   console.log((videoLink));
- });
+    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=YouTube+Data+API&type=video&videoCaption=closedCaption&key=${apiKey}`)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        let videoLink = "https://www.youtube.com/watch?v=" + data.items[0].id.videoId;
+        videoLinkEl.innerHTML = videoLink;
+        videoLinkEl.setAttribute("href", videoLink)
+        console.log((videoLink));
+    });
 
 
 fetch('https://en.wikipedia.org/w/api.php?action=query&prop=pageprops&titles=Finding_Dory&format=json&origin=*')
- .then(function (response) {
-   return response.json();
- })
- .then(function (data) {
-   console.log(data)
-    console.log(data.query.pages)
-    console.log(data.query.pages[Object.keys(data.query.pages)[0]].pageprops["wikibase-shortdesc"])
-    wikiFactEl.innerHTML = data.query.pages[Object.keys(data.query.pages)[0]].pageprops["wikibase-shortdesc"];
-  //  console.log((data.query.pages[239587].pageprops["wikibase-shortdesc"]));
- }); 
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+        console.log(data.query.pages)
+        console.log(data.query.pages[Object.keys(data.query.pages)[0]].pageprops["wikibase-shortdesc"])
+        wikiFactEl.innerHTML = data.query.pages[Object.keys(data.query.pages)[0]].pageprops["wikibase-shortdesc"];
+
+    }); 
+})
