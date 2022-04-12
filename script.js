a39e60f4ba6d862ad86e5a364d54e236cb4e4d2f
K4ne <nick@getmighty.net>
2022-04-12 13:26:48 -0500

    added local storage

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ let wikiFactEl = document.getElementById("wiki")
 let searchButton = document.querySelector("#search")
 let userInput = document.getElementById("select1")
 
-
+userInput.value = localStorage.getItem("history")
 
 function button() {
+    localStorage.setItem("history",userInput.value)
     fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${userInput.value}&type=video&videoCaption=closedCaption&key=${apiKey}`)
     .then(function (response) {
         return response.json();
@@ -15,7 +16,6 @@ function button() {
         let videoLink = "https://www.youtube.com/watch?v=" + data.items[0].id.videoId;
         videoLinkEl.innerHTML = videoLink;
         videoLinkEl.setAttribute("href", videoLink)
-        console.log((videoLink));
     });
 
 
@@ -25,13 +25,9 @@ fetch(`https://en.wikipedia.org/w/api.php?action=query&prop=pageprops&titles=${u
     })
     .then(function (data) {
         console.log(data)
-        console.log(data.query.pages)
         console.log(data.query.pages[Object.keys(data.query.pages)[0]].pageprops["wikibase-shortdesc"])
         wikiFactEl.innerHTML = data.query.pages[Object.keys(data.query.pages)[0]].pageprops["wikibase-shortdesc"];
 
     }); 
 
-function consolelog(event) {
-console.log(event.target.innerHTML)
-}
 }
